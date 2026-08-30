9b054e2296dd19b34c95cfa79f9cc152491c0b47
MateusPires11 <mateusoliveirapires509@gmail.com>
2026-08-29 22:38:00 -0300

    init commit

---
+++ script/scripts.js
@@ -0,0 +1,106 @@
+const products = [
+    {
+        id: 1,
+        title: "Smartphone Samsung Galaxy A54",
+        price: 1899.99,
+        discount: 15,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_660323-MLA99954016771_112025-F.webp"
+    },
+    {
+        id: 2,
+        title: "Notebook Lenovo IdeaPad",
+        price: 2999.90,
+        discount: 20,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_708611-MLA99896047685_112025-F.webp"
+    },
+    {
+        id: 3,
+        title: "Smart TV LG 50 4K",
+        price: 2299.00,
+        discount: 10,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_736986-MLA110508533913_042026-F.webp"
+    },
+    {
+        id: 4,
+        title: "Tênis Nike Air Max",
+        price: 399.99,
+        discount: 25,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_769580-MLB100977776357_122025-F-tnis-nike-air-max-excee-feminino.webp"
+    },
+    {
+        id: 5,
+        title: "Fone JBL Bluetooth",
+        price: 199.90,
+        discount: 30,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_668195-MLA99454647096_112025-F.webp"
+    },
+    {
+        id: 6,
+        title: "Cadeira Gamer ThunderX3",
+        price: 899.00,
+        discount: 18,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_901384-MLB93038853354_092025-F-cadeira-gamer-ergonomica-thunderx3-xtc-mesh-preta.webp"
+    },
+    {
+        id: 7,
+        title: "Console PlayStation 5",
+        price: 3999.99,
+        discount: 5,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_921596-MLA100042443481_122025-F-console-sony-playstation-5-edicao-slim-disk-1-tb-branco.webp"
+    },
+    {
+        id: 8,
+        title: "Apple Watch Series 8",
+        price: 2899.00,
+        discount: 12,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_947321-MLA99456212716_112025-F.webp"
+    },
+    {
+        id: 9,
+        title: "Tablet Samsung Galaxy Tab",
+        price: 2199.90,
+        discount: 15,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_784513-MLA107202361084_032026-F.webp"
+    },
+    {
+        id: 10,
+        title: "Câmera Canon EOS Rebel",
+        price: 2699.00,
+        discount: 8,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_667598-MLA114335978502_082026-F.webp"
+    },
+    {
+        id: 11,
+        title: "Mouse Gamer Logitech",
+        price: 249.90,
+        discount: 20,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_943111-MLA100072964389_122025-F.webp"
+    },
+    {
+        id: 12,
+        title: "Teclado Mecânico RGB",
+        price: 449.90,
+        discount: 22,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_981251-MLA108426406423_032026-F.webp"
+    }
+];
+
+
+
+function cardsprodutos(product){
+    return`
+        <div>
+            <img>
+            <h3></h3>
+        </div>
+    
+    
+    `
+
+} 
+
+
+
+document.addEventListener('DOMContentLoaded' ()=>{
+
+})
